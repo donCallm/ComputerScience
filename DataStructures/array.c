abb0fcb746a83c0547354d9b3ade8c545b4a4425
donCallm <nikitich-1998@bk.ru>
2023-03-02 21:51:13 +0300

    [HOTFIX] add methods my_remove and size

--- DataStructures/array.c
+++ DataStructures/array.c
@@ -13,6 +13,11 @@ void create_array(struct array *array)
     array->arr = (int*)malloc(0 * sizeof(int));
 }
 
+int size(struct array *array)
+{
+    return array->size;
+}
+
 void add(struct array *array, int number)
 {
     int *temp_array = (int*)malloc(array->size * sizeof(int));
@@ -27,6 +32,35 @@ void add(struct array *array, int number)
         array->arr[i] = temp_array[i];
     
     array->arr[array->size - 1] = number;
+}
+
+void my_remove(struct array *array, int index)
+{
+    if (array->size == 0)
+        return;
+
+    if (array->size < index)
+        return;
+
+    if (array->size == 1)
+    {
+        array->arr = NULL;
+        array->size = 0;
+        return;
+    }
+
+    int *temp_array = (int*)malloc(array->size * sizeof(int));
+
+    for (size_t i = 0; i < array->size; i++)
+        temp_array[i] = array->arr[i];
+
+    array->size = array->size - 1;
+    array->arr = (int*)malloc(array->size * sizeof(int));
+
+    for (size_t i = 0; i <= index; i++)
+        if (i == index)
+            for (size_t j = i + 1; j < array->size; j++)
+                array->arr[j - 1] = temp_array[j];
 }
 
 void delete(struct array *array)
@@ -48,6 +82,8 @@ int main()
     struct array *p_array = &array;
     create_array(p_array);
     add(p_array, 1);
+    print_arr(p_array->arr, p_array->size);
+    my_remove(p_array, 0);
     print_arr(p_array->arr, p_array->size);
     delete(p_array);
     
