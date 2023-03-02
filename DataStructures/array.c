4ea2de22739ff7ed2957242e55ce786493225537
donCallm <nikitich-1998@bk.ru>
2023-03-03 00:42:09 +0300

    [HOTFIX] fix add and remove

--- DataStructures/array.c
+++ DataStructures/array.c
@@ -18,28 +18,29 @@ int size(struct array *array)
     return array->size;
 }
 
+int is_valid(int size, int index)
+{
+    if (size == 0 || size < index)
+        return 1;
+    return 0;
+}
+
 void add(struct array *array, int number)
 {
-    int *temp_array = (int*)malloc(array->size * sizeof(int));
+    int *temp_array = (int*)malloc((array->size + 1) * sizeof(int));
 
     for (size_t i = 0; i < array->size; i++)
         temp_array[i] = array->arr[i];
     
+    free(array->arr);
     array->size = array->size + 1;
-    array->arr = (int*)malloc(array->size * sizeof(int));
-
-    for (size_t i = 0; i < array->size - 1; i++)
-        array->arr[i] = temp_array[i];
-    
+    array->arr = temp_array;
     array->arr[array->size - 1] = number;
 }
 
 void my_remove(struct array *array, int index)
 {
-    if (array->size == 0)
-        return;
-
-    if (array->size < index)
+    if (is_valid(array->size, index) >= 1)
         return;
 
     if (array->size == 1)
@@ -49,30 +50,28 @@ void my_remove(struct array *array, int index)
         return;
     }
 
-    int *temp_array = (int*)malloc(array->size * sizeof(int));
-
-    for (size_t i = 0; i < array->size; i++)
-        temp_array[i] = array->arr[i];
-
-    array->size = array->size - 1;
-    array->arr = (int*)malloc(array->size * sizeof(int));
+    int *temp_array = (int*)malloc((array->size - 1) * sizeof(int));
 
     for (size_t i = 0; i <= index; i++)
         if (i == index)
             for (size_t j = i + 1; j < array->size; j++)
-                array->arr[j - 1] = temp_array[j];
+                temp_array[j] = array->arr[j + 1];
+
+    free(array->arr);
+    array->size = array->size - 1;
+    array->arr = temp_array;
 }
 
 void delete(struct array *array)
 {
-    free(array->arr);
+    array = NULL;
 }
 
-void print_arr(int arr[], int arr_size)
+void print_arr(struct array *array)
 {
     printf("Array: ");
-    for (size_t i = 0; i < arr_size; i++)
-        printf("%d ", arr[i]);
+    for (size_t i = 0; i < array->size; i++)
+        printf("%d ", array->arr[i]);
     printf("\n");
 }
 
@@ -80,11 +79,11 @@ int main()
 {
     struct array array;
     struct array *p_array = &array;
-    create_array(p_array);
+    create_array(p_array); 
     add(p_array, 1);
-    print_arr(p_array->arr, p_array->size);
+    print_arr(p_array);
     my_remove(p_array, 0);
-    print_arr(p_array->arr, p_array->size);
+    print_arr(p_array);
     delete(p_array);
     
     return 0;
