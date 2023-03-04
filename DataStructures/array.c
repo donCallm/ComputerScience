cee4246695890e13583e8bdfd0748127c5e05ad9
donCallm <nikitich-1998@bk.ru>
2023-03-04 19:18:22 +0300

    [HOTFIX] fix remove and add

--- DataStructures/array.c
+++ DataStructures/array.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<malloc.h>
+#include <stdlib.h>
 
 struct array
 {
@@ -7,93 +8,83 @@ struct array
     int *arr;
 };
 
-void create_array(struct array *array)
+void create_array(struct array *arr)
 {
-    array->size = 0;
-    array->arr = (int*)malloc(0 * sizeof(int));
+    arr->size = 0;
+    arr->arr = (int*)malloc(0 * sizeof(int));
 }
 
-int size(struct array *array)
+int size(struct array *arr)
 {
-    return array->size;
+    return arr->size;
 }
 
-int is_valid(int size, int index)
+int is_valid_index(int size, int index)
 {
     if (size == 0 || size < index)
         return 1;
     return 0;
 }
 
-void add(struct array *array, int number)
+void add(struct array *arr, int number)
 {
-    int *temp_array = (int*)malloc((array->size + 1) * sizeof(int));
-
-    for (size_t i = 0; i < array->size; i++)
-        temp_array[i] = array->arr[i];
-    
-    free(array->arr);
-    array->size = array->size + 1;
-    array->arr = temp_array;
-    array->arr[array->size - 1] = number;
+    arr->arr = realloc(arr->arr, (arr->size + 1) * sizeof(int));
+    arr->size = arr->size + 1;
+    arr->arr[arr->size - 1] = number;
 }
 
-void my_remove(struct array *array, int index)
+void my_remove(struct array *arr, int index)
 {
-    if (is_valid(array->size, index) >= 1)
+    if (is_valid_index(arr->size, index) >= 1)
         return;
 
-    if (array->size == 1)
+    if (arr->size == 1)
     {
-        array->arr = NULL;
-        array->size = 0;
+        free(arr);
         return;
     }
 
-    int *temp_array = (int*)malloc((array->size - 1) * sizeof(int));
-
-    for (size_t i = 0; i <= index; i++)
+    for (size_t i = 0; i <= arr->size; i++)
     {
-        temp_array[i] = array->arr[i];
-        if (i == index)
-            for (size_t j = i + 1; j < array->size; j++)
-                temp_array[j] = array->arr[j + 1];
+        if (i > index)
+            arr->arr[i - 1] = arr->arr[i];
     }
 
-    free(array->arr);
-    array->size = array->size - 1;
-    array->arr = temp_array;
+    arr->arr = realloc(arr->arr, (arr->size - 1) * sizeof(int));
+    arr->size = arr->size - 1;
 }
 
-void delete(struct array *array)
+void delete(struct array *arr)
 {
-    array = NULL;
+    free(arr);
 }
 
-void filling_array(struct array *array)
+void filling_array(struct array *arr)
 {
     for (size_t i = 0; i < 100; i++)
-        add(array, i);
+        add(arr, i);
 }
 
-void print_arr(struct array *array)
+void print_arr(struct array *arr)
 {
     printf("Array: ");
-    for (size_t i = 0; i < array->size; i++)
-        printf("%d ", array->arr[i]);
+    for (size_t i = 0; i < arr->size; i++)
+    {
+        int nubmer = arr->arr[i];
+        printf("%d ", arr->arr[i]);
+    }
     printf("\n");
 }
 
 int main()
 {
-    struct array array;
-    struct array *p_array = &array;
-    create_array(p_array);
-    filling_array(p_array);
-    print_arr(p_array);
-    my_remove(p_array, 6);
-    print_arr(p_array);
-    delete(p_array);
+    struct array *p_arr = (struct array*)malloc(sizeof(struct array));
+    create_array(p_arr);
+    filling_array(p_arr);
+    print_arr(p_arr);
+    my_remove(p_arr, 6);
+    print_arr(p_arr);
+    delete(p_arr);
     
     return 0;
 }
