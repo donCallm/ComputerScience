392ec773aec416b51c2a778153eacebb1490cdc1
donCallm <nikitich-1998@bk.ru>
2023-03-20 00:35:20 +0300

    [HOTFIX] fix leak

--- DataStructures/linked_list/node.c
+++ DataStructures/linked_list/node.c
@@ -5,11 +5,11 @@ struct node* create_node()
 {
     struct node* nd = (struct node*)malloc(sizeof(struct node));
     nd->data = 0;
+    nd->next = NULL;
     return nd;
 }
 
 void delete_node(struct node *nd)
 {
-    free(nd->next);
     free(nd);
 }
