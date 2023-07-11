924db0d56ed9f21cfc6b83194d85f858b0fcddaf
donCallm <nikitich-1998@bk.ru>
2023-07-11 23:46:26 +0300

    [HOTFIX] fix coments

--- Algorithms/prefix_array.cpp
+++ Algorithms/prefix_array.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> prefix_array(const std::vector<int>& arr)
+std::vector<int> prefix_array(const std::vector<int>& arr, int arr_size)
 {
-    std::vector<int> prefix_arr(arr.size() + 1);
+    std::vector<int> prefix_arr(arr_size+ 1);
 
     prefix_arr[0] = 0;
-    for (int i = 0; i < arr.size() + 1; ++i)
+    for (int i = 0; i < arr_size + 1; ++i)
         prefix_arr[i + 1] += prefix_arr[i] + arr[i];
     
     return prefix_arr;
 }
 
-void show_prefix_arr(const std::vector<int>& prefix_arr)
+void show_prefix_arr(const std::vector<int>& prefix_arr, int arr_size)
 {
     for (int i = 0; i < prefix_arr.size(); ++i)
         std::cout << prefix_arr[i] << " ";
@@ -20,8 +20,9 @@ void show_prefix_arr(const std::vector<int>& prefix_arr)
 
 int main()
 {
+    int arr_size = 10;
     std::vector<int> arr {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         
-    show_prefix_arr(prefix_array(arr));
+    show_prefix_arr(prefix_array(arr, arr_size), arr_size);
     return 0;
 }
