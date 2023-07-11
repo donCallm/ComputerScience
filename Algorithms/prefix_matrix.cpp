504c2d7a4ed313ac3f0d23db54fce15d25af814e
donCallm <nikitich-1998@bk.ru>
2023-07-11 21:46:31 +0300

    [HOTFIX] fix comments

--- Algorithms/prefix_matrix.cpp
+++ Algorithms/prefix_matrix.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <vector>
 
-
-using namespace std;
-
-vector<vector<int>> prefix_matrix(int x_size, int y_size, vector<vector<int>> matrix)
+std::vector<std::vector<int>> prefix_matrix(int x_size, int y_size, const std::vector<std::vector<int>>& matrix)
 {
-    vector<vector<int>> prefix_matrix(x_size);
+    std::vector<std::vector<int>> prefix_matrix(x_size);
 
     for (int i = 0; i < y_size; ++i)
         prefix_matrix[i].resize(x_size);
@@ -17,23 +14,13 @@ vector<vector<int>> prefix_matrix(int x_size, int y_size, vector<vector<int>> ma
         {
             if(i == 0)
             {
-                if (j == 0)
-                {
-                    prefix_matrix[i][j] = matrix[i][j]; 
-                    continue;
-                }
-
-                prefix_matrix[i][j] = matrix[i][j] + matrix[i][j - 1];
+                if (j == 0) prefix_matrix[i][j] = matrix[i][j]; 
+                else prefix_matrix[i][j] = matrix[i][j] + matrix[i][j - 1];
             }
             else
             {
-                if (j == 0)
-                {
-                    prefix_matrix[i][j] = matrix[i][j] + matrix[i - 1][j];
-                    continue;
-                }
-                
-                prefix_matrix[i][j] = matrix[i][j] + prefix_matrix[i - 1][j] + prefix_matrix[i][j - 1];
+                if (j == 0) prefix_matrix[i][j] = matrix[i][j] + matrix[i - 1][j];
+                else prefix_matrix[i][j] = matrix[i][j] + prefix_matrix[i - 1][j] + prefix_matrix[i][j - 1];
             }
         }
     }
@@ -41,14 +28,14 @@ vector<vector<int>> prefix_matrix(int x_size, int y_size, vector<vector<int>> ma
     return prefix_matrix;
 }
 
-void show_prefix_matrix(int x_size, int y_size, vector<vector<int>> prefix_matrix)
+void show_prefix_matrix(int x_size, int y_size, const std::vector<std::vector<int>>& prefix_matrix)
 {
-    cout << "Prefix matrix: " << endl;
+    std::cout << "Prefix matrix: " << std::endl;
     for (int i = 0; i < y_size; ++i)
     {
         for (int j = 0; j < x_size; ++j)
-            cout << prefix_matrix[i][j] << " ";
-        cout << endl;
+            std::cout << prefix_matrix[i][j] << " ";
+        std::cout << std::endl;
     }
 }
 
@@ -56,17 +43,19 @@ int main()
 {
     int y_size = 3;
     int x_size = 3;
-    vector<vector<int>> matrix(y_size);
+    std::vector<std::vector<int>> matrix(y_size);
 
     for (int i = 0; i < y_size; ++i)
         matrix[i].resize(x_size);
     
     for (int i = 0; i < y_size; ++i)
+    {
         for (int j = 0; j < x_size; ++j)
         {
             matrix[i][j] = j;
-            cout << matrix[i][j] << endl;
+            std::cout << matrix[i][j] << std::endl;
         }
+    }
     
     show_prefix_matrix(x_size, y_size, prefix_matrix(x_size, y_size, matrix));
 
--- Algorithms/xor_arr.cpp
+++ Algorithms/xor_arr.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-vector<int> xor_arr(vector<int> arr1, vector<int> arr2, int arr_size)
+std::vector<int> xor_arr(const std::vector<int>& arr1, std::vector<int>& arr2, int arr_size)
 {
-    vector<int> xor_arr (arr_size);
+    std::vector<int> xor_arr (arr_size);
 
     for (int i = 0; i < arr_size; ++i)
+    {
         if (arr1[i] == arr2[i]) xor_arr[i] = 0;
         else xor_arr[i] = 1;
+    }
 
     return xor_arr;
 }
 
-void show_xor_arr(vector<int> xor_arr, int arr_size)
+void show_xor_arr(const std::vector<int>& xor_arr, int arr_size)
 {
-    cout << "Xor array: ";
+    std::cout << "Xor array: ";
     for (int i = 0; i < arr_size; ++i)
-        cout << xor_arr[i] << " ";
+        std::cout << xor_arr[i] << " ";
 }
 
 int main()
 {
     int arr_size = 10;
-    vector<int> arr1{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> arr2{0, 5, 2, 3, 1, 1, 6, 7, 5, 9};
+    std::vector<int> arr1{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> arr2{0, 5, 2, 3, 1, 1, 6, 7, 5, 9};
     
     show_xor_arr(xor_arr(arr1, arr2, arr_size), arr_size);
     return 0;
